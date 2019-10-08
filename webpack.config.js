b9e748a6759d46167eb3c205da973233c2abd4a7
John Ja Garcia <jhon.garcia@softwareestrategico.co>
2019-10-08 14:27:29 -0500

    Restore index.html template

--- webpack.config.js
+++ webpack.config.js
@@ -108,6 +108,19 @@ if (process.env.NODE_ENV === 'production') {
     new webpack.LoaderOptionsPlugin({
       minimize: true
     }),
-    new HtmlWebpackPlugin()
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+        // more options:
+        // https://github.com/kangax/html-minifier#options-quick-reference
+      },
+      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+      chunksSortMode: 'dependency'
+    })
   ])
 }
